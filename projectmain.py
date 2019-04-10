3e8e2378166435327f2bbc895353f0784fabc081
Kathryn Saldivar <kisaldivar1995@gmail.com>
2019-04-10 16:39:42 -0700

    Updated docstrings for basic functions

--- projectmain.py
+++ projectmain.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 10 15:39:35 2019
+Main Python Code
 
 @authors:
     Kathryn Saldivar 108578476
@@ -11,18 +11,22 @@ Created on Wed Apr 10 15:39:35 2019
 class person:
     def __init__(self,p,i):
         """
+            person(p,i)
             personality is a number that correlates to a table with the personality information
             investment is the number of hours a week the person is willing to invest in a relationship
         """
         self.personality = p
         self.investment = i
         
-    def pType(self):
+    def getPtype(self):
         return self.personality
+    def getInvestment(self):
+        return self.investment
     
 class friendship:
     def __init__(self,f1,f2,l=0,d=0):
         """
+            friendship(f1,f2,l,d)
             f1 is the first person in a friendship
             f2 is the second person in a friendship
             l is the length of time they have been friends. Defaults to zero if doing a new friendship.
@@ -36,11 +40,12 @@ class friendship:
         self.calculateFV()
         
     def calculateFV(self):
+        """
+            Uses all the variables to create a number between 1-100 which determines how likely a friendship will survive hardship
+        """
         self.friendshipValue = 0
         
     def getFV(self):
         return self.friendshipValue
        
-
-        
 
